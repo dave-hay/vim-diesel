a5ba0e16e71b9c909aef6ece391ac30cfe580793
hayitsdavid <davehay93@gmail.com>
2022-06-04 15:46:44 -0700

    have to have leader in alpha-nvim

--- lua/config/alpha-nvim.lua
+++ lua/config/alpha-nvim.lua
@@ -14,9 +14,9 @@ dashboard.section.header.val = {
 
 -- Set menu
 dashboard.section.buttons.val = {
-	dashboard.button("SPC f f", "  > Find file", ":Leaderf file --popup<CR>"),
-	dashboard.button("SPC f r", "  > Recent files", ":Leaderf mru --popup<CR>"),
-	dashboard.button("SPC f g", "  > Project grep", ":Leaderf rg --popup<CR>"),
+	dashboard.button("Leader f f", "  > Find file", ":Leaderf file --popup<CR>"),
+	dashboard.button("Leader f r", "  > Recent files", ":Leaderf mru --popup<CR>"),
+	dashboard.button("Leader f g", "  > Project grep", ":Leaderf rg --popup<CR>"),
 	dashboard.button("u", "  > Update plugins", ":PlugUpdate<CR>"),
 	dashboard.button("e", "  > New file", ":enew <CR>"),
 	dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
@@ -24,14 +24,7 @@ dashboard.section.buttons.val = {
 
 local fortune = require("alpha.fortune")
 dashboard.section.footer.val = fortune()
+alpha.setup(dashboard.opts)
 
 -- Send config to alpha
 alpha.setup(dashboard.opts)
-
--- button("e", "  New file", "<cmd>ene <CR>"),
--- button("SPC f f", "  Find file"),
--- button("SPC f h", "  Recently opened files"),
--- button("SPC f r", "  Frecency/MRU"),
--- button("SPC f g", "  Find word"),
--- button("SPC f m", "  Jump to bookmarks"),
--- button("SPC s l", "  Open last session"),
