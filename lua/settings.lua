bd3432d172c48527eabeac18c635336c63355de3
hayitsdavid <davehay93@gmail.com>
2022-07-04 09:58:12 -0700

    global settings

---
+++ lua/settings.lua
@@ -0,0 +1,24 @@
+local o = vim.o
+-- local wo = vim.wo
+-- local bo = vim.bo
+
+-- global options
+o.noerrorbells = true
+o.nohlsearch = true
+o.scrolloff = 8
+o.signcolumn = true
+o.tabstop = 2
+o.softtabstop = 2
+o.shiftwidth = 2
+o.expandtab = true
+o.smartindent = true
+o.relativenumber = true
+o.nu = true
+-- can navigate away from buffer and it doesn't delete
+o.hidden = true
+o.noswapfile = true
+o.nobackup = true
+-- o.clipboard+=unnamedplus
+o.cc = 80
+o.pumheight = 5
+-- o.guifont=Fira_Code:h18 " for firenvim
