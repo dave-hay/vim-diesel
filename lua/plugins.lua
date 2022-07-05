8ab18f42adb6827c94c2f4c977cae3b6692310be
hayitsdavid <davehay93@gmail.com>
2022-07-04 20:16:20 -0700

    fixing cmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,10 @@
+-----------------------------------------------------------
+-- Plugin manager configuration file
+-----------------------------------------------------------
+
 local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
 if fn.empty(fn.glob(install_path)) > 0 then
 	packer_bootstrap = fn.system({
 		"git",
@@ -9,38 +14,60 @@ if fn.empty(fn.glob(install_path)) > 0 then
 		"https://github.com/wbthomason/packer.nvim",
 		install_path,
 	})
+	vim.o.runtimepath = vim.fn.stdpath("data") .. "/site/pack/*/start/*," .. vim.o.runtimepath
 end
 
--- vim.cmd [[packadd packer.nvim]]
+-- Autocommand that reloads neovim whenever you save the packer_init.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost packer_init.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
 
 return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim") -- packer can manage itself
+
+	-- lsp
 	use({
 		"williamboman/nvim-lsp-installer",
 		"neovim/nvim-lspconfig",
 	})
 
-	use("neovim/nvim-lspconfig")
+	-- Treesitter interface
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
 	})
+
 	use("cohama/lexima.vim")
 	use("sbdchd/neoformat")
-	-- Auto complete
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/nvim-cmp")
+
+	-- Autocomplete
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			-- Snippits
+			"L3MON4D3/LuaSnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-buffer",
+			"saadparwaiz1/cmp_luasnip",
+		},
+	})
+
 	use("onsails/lspkind.nvim")
-	-- Snippits
-	use("L3MON4D3/LuaSnip")
-	use("saadparwaiz1/cmp_luasnip")
+
 	-- comment
 	use("tpope/vim-commentary/")
 	use("JoosepAlviste/nvim-ts-context-commentstring")
-	-- use 'hrsh7th/cmp-vsnip'
-	-- use 'hrsh7th/vim-vsnip'
 	use({
 		"fatih/vim-go",
 		run = ":GoUpdateBinaries",
@@ -74,6 +101,8 @@ return require("packer").startup(function(use)
 	-- use 'morhetz/gruvbox'
 	use("kyazdani42/nvim-web-devicons")
 
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
 	if packer_bootstrap then
 		require("packer").sync()
 	end
