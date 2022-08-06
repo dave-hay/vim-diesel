9f2dfe0332a05854b5c0669dd2d74dd6796e157e
hayitsdavid <davehay93@gmail.com>
2022-08-06 00:27:35 -0700

    telescope ignore dotbot

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
-require('telescope').setup{
-  defaults = {
-    file_ignore_patterns = {"node_modules", "venv"}
-  }
-}
+require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = { "node_modules", "venv", "dotbot" },
+	},
+})
