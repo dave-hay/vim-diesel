01f6c93ad7318affdaca052185a75835c58e8cb2
hayitsdavid <davehay93@gmail.com>
2022-08-11 23:50:08 -0700

    neoformatter config

--- lua/config.lua
+++ lua/config.lua
@@ -2,20 +2,20 @@ require("impatient")
 --
 -- Plugins configs
 --
-require("nvim-lsp-installer").setup{automatic_installation = true}
-require('plugins.lsp_cmp')
-
+require("nvim-lsp-installer").setup({ automatic_installation = true })
+require("plugins.lsp_cmp")
+require("plugins.neoformat")
 -- theme
 -- https://github.com/folke/tokyonight.nvim
 vim.g.tokyonight_style = "night"
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_transparent = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-vim.cmd[[colorscheme tokyonight]]
+vim.cmd([[colorscheme tokyonight]])
 require("lualine").setup({
- options = {
- theme = "onedark",
- },
+	options = {
+		theme = "onedark",
+	},
 })
 
 -- if onedark wanted
--- lua/plugins/neoformat.lua
+++ lua/plugins/neoformat.lua
@@ -1,13 +1,6 @@
-local g = vim.g -- global variables
-
-g.neoformat_json_prettierd = {
-   'exe': 'prettierd',
-}
-
---[[ local prettier = {
-  formatCommand = 'prettierd "${INPUT}"',
-  formatStdin = true,
-  env = {
-    string.format('PRETTIERD_DEFAULT_CONFIG=%s', vim.fn.expand('~/.config/nvim/utils/linter-config/.prettierrc.json')),
-  },
-} ]]
+vim.cmd([[
+let g:neoformat_enabled_json = ['prettierd', 'prettier']
+let g:neoformat_enabled_javascript = ['prettierd', 'prettier']
+let g:neoformat_enabled_python = ['yapf']
+let g:neoformat_enabled_svelte = ['prettierd']
+]])
