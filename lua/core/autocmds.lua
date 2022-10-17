a37be9953787d35bcb70b2e7c4a6598723fd9994
hayitsdavid <davehay93@gmail.com>
2022-10-17 14:46:05 -0700

    cmp_nvm_lsp update

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -2,7 +2,7 @@ local utils = require("core.utils")
 
 local groups = {
 	packerSyncOnSave = { "BufWritePost packer_config.lua source <afile> | PackerSync" },
-	neoformat = { "BufWritePre * undojoin | Neoformat" },
+	neoformat = { "BufWritePre * silent! undojoin | Neoformat" },
 }
 
 for name, aucmd in pairs(groups) do
--- lua/plugins/lsp_cmp.lua
+++ lua/plugins/lsp_cmp.lua
@@ -1,7 +1,7 @@
 -- load nvim-cmp config
 require("plugins.cmp")
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local util = require("lspconfig/util")
 local lspconfig = require("lspconfig")
