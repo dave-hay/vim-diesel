2310b690b131c9738d431a4d1446e82d847b103b
Dave Hay <davehay93@gmail.com>
2022-12-04 20:25:15 -0800

    python formatter

--- lua/plugins/neoformat.lua
+++ lua/plugins/neoformat.lua
@@ -1,7 +1,7 @@
 vim.cmd([[
 let g:neoformat_enabled_json = ['prettierd', 'prettier']
 let g:neoformat_enabled_javascript = ['prettierd', 'prettier']
-let g:neoformat_enabled_python = ['yapf']
+let g:neoformat_enabled_python = ['black']
 let g:neoformat_enabled_svelte = ['prettierd']
 let g:neoformat_enabled_c = ['clangformat']
 ]])
