5369e60227cff254d2fff5a901ede6654e7b9850
Dave Hay <davehay93@gmail.com>
2023-05-04 18:53:34 -0700

    new nerd font updated icons

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -14,12 +14,12 @@ dashboard.section.header.val = {
 
 -- Set menu
 dashboard.section.buttons.val = {
-	dashboard.button("Leader f f", "  > Find file", ":Telescope find_files<CR>", nil),
-	dashboard.button("Leader f r", "  > Recent files", ":Telescope oldfiles<CR>", nil),
-	dashboard.button("Leader f g", "  > Project grep", ":Telescope live_grep<CR>", nil),
-	dashboard.button("u", "  > Update plugins", ":PackerSync<CR> <bar> :PackerCompile<CR>", nil),
-	dashboard.button("e", "  > New file", ":enew <CR>", nil),
-	dashboard.button("q", "  > Quit NVIM", ":qa<CR>", nil),
+	dashboard.button("Leader f f", "󰍉  Find file", ":Telescope find_files<CR>", nil),
+	dashboard.button("Leader f r", "󰅐  Recent files", ":Telescope oldfiles<CR>", nil),
+	dashboard.button("Leader f g", "󰓾  Project grep", ":Telescope live_grep<CR>", nil),
+	dashboard.button("u", "󰚰  Update plugins", ":PackerSync<CR> <bar> :PackerCompile<CR>", nil),
+	dashboard.button("e", "  New file", ":enew <CR>", nil),
+	dashboard.button("q", "󰩈  Quit NVIM", ":qa<CR>", nil),
 }
 
 local fortune = require("alpha.fortune")
