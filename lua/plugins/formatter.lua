c9b553952afc7add624c655a1aafdc1c11484353
David <david@MacBook-Air.local>
2023-11-29 16:32:53 -0800

    bash format

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -10,6 +10,23 @@ function f.prettierd()
 		stdin = true,
 	}
 end
+function f.beautysh()
+	local shiftwidth = vim.opt.shiftwidth:get()
+	local expandtab = vim.opt.expandtab:get()
+
+	if not expandtab then
+		shiftwidth = 0
+	end
+
+	return {
+		exe = "beautysh",
+		args = {
+			"-i",
+			shiftwidth,
+			util.escape_path(util.get_current_buffer_file_path()),
+		},
+	}
+end
 
 require("formatter").setup({
 	-- Enable or disable logging
@@ -31,11 +48,13 @@ require("formatter").setup({
 		python = {
 			require("formatter.filetypes.python").black,
 		},
+		sh = { f.beautysh },
 		svelte = {
 			require("formatter.filetypes.svelte").prettier,
 		},
 		typescript = { f.prettierd },
 		typescriptreact = { f.prettierd },
+		zsh = { f.beautysh },
 		-- Use the special "*" filetype for defining formatter configurations on
 		-- any filetype
 		["*"] = {
--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,12 +1,25 @@
-  require("mason-lspconfig").setup({
-    -- Here you can specify which LSP servers you want to have installed by default
-    ensure_installed = { "pyright", "tsserver", "lua_ls", "rust_analyzer","marksman" },
-    -- Add any other configuration options for mason-lspconfig here
-  })
+require("mason-lspconfig").setup({
+	-- Here you can specify which LSP servers you want to have installed by default
+	ensure_installed = {
+		-- LSPs
+		"pyright",
+		"tsserver",
+		"lua_ls",
+		"rust_analyzer",
+		-- Formatters
+		--[[ "beautysh",
+		"black",
+		"marksman",
+		"prettier",
+		"prettierd",
+		"stylua", ]]
+	},
+	-- Add any other configuration options for mason-lspconfig here
+})
 
-  -- Setup LSP servers using nvim-lspconfig
-  --[[ local lspconfig = require('lspconfig')
+-- Setup LSP servers using nvim-lspconfig
+--[[ local lspconfig = require('lspconfig')
 
   lspconfig.pyright.setup{}  -- Example for Python
   lspconfig.tsserver.setup{} -- Example for TypeScript ]]
-  -- Add more LSP server configurations here
+-- Add more LSP server configurations here
