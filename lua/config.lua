0789825eb014abe066fe0ddb39ef2fe621cf403f
dave-hay <davehay93@gmail.com>
2025-03-30 16:04:50 -0700

    update config

--- lua/config.lua
+++ lua/config.lua
@@ -1,12 +1,9 @@
 --
 -- Plugins configs
 --
-require("mason").setup()
-require("mason-lspconfig").setup()
 require("plugins.lsp_cmp")
 require("plugins.neoformat")
 -- require("plugins.formatter")
 -- theme
 require("plugins.tokyo")
-require("plugins.lualine")
 vim.cmd([[colorscheme tokyonight]])
